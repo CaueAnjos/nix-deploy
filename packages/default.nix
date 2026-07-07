39e421b7077f4633397a08e43d97a99991fdc05a
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-07-07 15:09:13 -0300

    fix(packages): simplify closure test with mkClosure

--- packages/default.nix
+++ packages/default.nix
@@ -12,19 +12,10 @@
       inherit (pkgs) patchelf;
       patchstrings = pkgs.callPackage ./patchstrings.nix {};
 
-      hello-closure = pkgs.runCommand "hello-closure" {
-        nativeBuildInputs = [
-          (pkgs.deployTools.mkCopyclosureCommand {
-            drv = pkgs.hello;
-            refs = pkgs.deployTools.mkRuntimeDeps pkgs.hello;
-          })
-        ];
-      } ''copyclosure "$out"'';
-
+      test-closure = pkgs.deployTools.mkClosure pkgs.hello;
       test-bundle = pkgs.deployTools.mkBundle {
         drv = pkgs.hello;
       };
-
       test-compact = pkgs.deployTools.mkCompactClosure pkgs.hello;
       test-references = pkgs.deployTools.mkReferences {
         drv = pkgs.hello;
